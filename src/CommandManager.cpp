6d7342dde76631f211dec9055e6fdf42f7501e9f
hamiz262007 <hamiz262007@gmail.com>
2026-05-02 17:02:03 +0500

    class manager command.

--- src/CommandManager.cpp
+++ src/CommandManager.cpp
@@ -16,6 +16,38 @@ public:
 		Folder* newFolder = new Folder(name, currentFolder);
 		currentFolder->addNode(newFolder);
 	}
+	void cd(string name) {
+		Node** mylist = currentFolder->getList();
+		bool found = false;	
+		
+		if (name == "..") {
+			currentFolder = (Folder*)currentFolder->getPreviousNode();
+			return;
+		}
+
+		for (int i = 0; i < currentFolder->getCount(); i++)
+		{
+			if (name == mylist[i]->getName()) {
+				Folder* temp = dynamic_cast<Folder*>mylist[i];
+				if (temp == nullptr)
+				{
+					cout << "Not a folder" << endl; break;
+				}
+				else
+				{
+					currentFolder = temp;
+					found = true;
+					
+				}
+				break;
+			}
+		}
+		if (found == false)
+		{
+			cout << "not found" << endl;
+		}
+	}
+
 private:
 
 };
