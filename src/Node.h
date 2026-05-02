a0864da4f81643c321829117820d71b766fcc13e
Hamiz <hamiz262007@gmail.com>
2026-05-02 14:55:02 +0500

    Node abstracts clas

--- src/Node.h
+++ src/Node.h
@@ -1,1 +1,23 @@
-#pragma once
+#include <iostream>
+using namespace std;
+class Node
+{
+	string name;
+	Node* prev;
+public:
+	Node(string fname, Node* p) {
+		name = fname;
+		prev = p;
+	}
+	virtual void open() = 0;
+	virtual void create() = 0;
+	virtual void remove() = 0;
+	virtual ~Node() {}
+	string getName() {
+		return name;
+	}
+	Node* getPreviousNode() {
+		return prev;
+	}
+
+};
